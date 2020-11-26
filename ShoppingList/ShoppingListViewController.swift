891a9b076932b3284d3f0e975b6ad5491a1f6af7
Dan Negreiros <negreiros.dev@hotmail.com>
2020-11-26 01:13:07 -0300

    Shopping list: adicionados datasources e delegate methods relativos a tableview

---
+++ ShoppingList/ShoppingListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Daniel Negreiros on 25/11/20.
+//
+
+import UIKit
+
+class ShoppingListViewController: UITableViewController {
+
+    let itemArray = ["Frios", "Bebidas", "Carnes", "Matinal", "Feira", "Temperos"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.none{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
